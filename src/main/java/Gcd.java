022e5f2f5d5ed5ce15977f0aaa8a81c187dae7a7
cwallin <cawallin@gmail.com>
2013-09-06 18:04:02 -0400

    First code and steps of TDD

---
+++ src/main/java/Gcd.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2013, Hadapt, Inc. All rights reserved.
+ */
+
+
+package main.java;
+
+public class Gcd {
+}
---
+++ src/main/test/GCDTest.java
@@ -0,0 +1,12 @@
+/*
+ * Christina Wallin, 2013.
+ */
+
+import org.junit.Test;
+
+public class GCDTest {
+  @Test
+  public void shouldOutputGcdFile(){
+    Gcd gcd = new Gcd();
+  }
+}
